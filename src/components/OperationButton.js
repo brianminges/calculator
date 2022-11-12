fa7b76ee88694e302d4642a90b542ee2a1c871a3
brianminges <btminges@gmail.com>
2022-11-12 16:57:49 -0600

    css set

--- src/components/OperationButton.js
+++ src/components/OperationButton.js
@@ -3,6 +3,7 @@ import { ACTIONS } from "./Calculator"
 export const OperationButton = ({ dispatch, operation }) => {
     return (
             <button 
+                id="operation-button"
                 onClick={() => dispatch({ type: ACTIONS.CHOOSE_OPERATION, payload: { operation } })}
             >
                 {operation}
